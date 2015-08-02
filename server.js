4f4b8a835fa4831d5bc75c00f2dcbc0e64fdd1ba
Caue Rego <caue@cregox.com>
2015-08-02 19:40:12 -0300

    add spec to assert "Hello World" and adapt server
    we’re not going to use c9 for now anymore. and it also works!

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(process.env.PORT, process.env.IP);
+var express = require('express');
+var app     = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -2,13 +2,22 @@ var request = require("request");
 
 var base_url = "http://localhost:3000/"
 
-describe("basiux server", function(){
-  describe("GET /", function(){
-    it("returns status code 200", function(){
+describe("basiux server", function() {
+  describe("GET /", function() {
+
+    it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
+
+    it("returns Hello World", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(body).toBe("Hello World");
+        done();
+      });
+    });
+
   });
 });
